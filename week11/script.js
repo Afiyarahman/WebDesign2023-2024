c657d0ffd8e44bc192008c3986cf188d0668f291
afiya <afiyarahman224@gmail.com>
2023-12-07 16:45:59 +0000

    added the button function and added the color

--- week11/script.js
+++ week11/script.js
@@ -3,18 +3,31 @@ function myClickFunction() {
     var headingElement = document.getElementById('pageheading');
     headingElement.firstChild.nodeValue = 'New heading';
 }
+function changeBackgroundColor() {
+    var buttonElement = document.getElementById('colorButton');
+    buttonElement.style.backgroundColor = 'blue';
+}
 // this functions detects the clicking on the paragraph
-function myLoadFunction_2() {
+/*function myLoadFunction_2() {
     var paragraphElement = document.getElementById('paragraph');
     paragraphElement.addEventListener('click', myClickFunction);
 }
 
 // this function detects the clicking on the heading
-/*function myLoadFunction() {
+function myLoadFunction() {
     var element = document.getElementById('pageheading');
     element.addEventListener('click', myClickFunction);
 }
 */
+
+function myLoadFunction_2() {
+    var paragraphElement = document.getElementById('paragraph');
+    paragraphElement.addEventListener('click', myClickFunction);
+
+    var buttonElement = document.getElementById('colorButton');
+    buttonElement.addEventListener('click', changeBackgroundColor);
+}
+
 
 
 document.addEventListener('DOMContentLoaded', myLoadFunction_2);
